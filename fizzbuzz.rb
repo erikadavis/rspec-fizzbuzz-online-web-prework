bd7b4a2a06027f88a4f4a0b3a15ff352fc40e24b
Erika Davis <davis2erika@gmail.com>
2019-04-08 23:05:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(3)
+  if fizz_3 == fizzbuzz(3)
+    return "Fizz"
+  end
+  if fizz_5 == fizzbuzz(5)
+    return "Buzz"
+  end
+  if fizz_15 == fizzbuzz(15)
+    return "Fizzbuzz"
+  else
+    return "nill"
+  end
+end
