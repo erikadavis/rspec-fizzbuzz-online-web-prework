35f6c0d51702bec28f8b28d508f8f2fa6a417106
Erika Davis <davis2erika@gmail.com>
2019-04-08 23:10:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,3 @@
 def fizzbuzz(3)
-  if fizz_3 == fizzbuzz(3)
-    return "Fizz"
-  end
-  if fizz_5 == fizzbuzz(5)
-    return "Buzz"
-  end
-  if fizz_15 == fizzbuzz(15)
-    return "Fizzbuzz"
-  else
-    return "nill"
-  end
+
 end
